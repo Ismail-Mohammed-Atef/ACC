9f21639bf213989e18e211f03ada6870f425900b
Gaber Raafat <gaberraffat8@gmail.com>
2025-03-10 00:30:50 +0200

    Adding Project controller and project Repositories (#6)

---
+++ BusinessLogic/Repository/RepositoryInterfaces/IProjetcRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataLayer.Models;
+
+namespace BusinessLogic.Repository.RepositoryInterfaces
+{
+    public interface IProjetcRepository : IGenericRepository<Project>
+    {
+    }
+}
