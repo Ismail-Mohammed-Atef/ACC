77a8195e3be2e90b3ddbbff6d3fbbd9396843fc5
Ahmed Nasr <amednasr164@gmail.com>
2025-03-11 12:31:38 +0200

    RepoAdded

--- ACC/Program.cs
+++ ACC/Program.cs
@@ -32,7 +32,7 @@ namespace ACC
 
 
 
-            //hello from ahmed
+           
 
 
 
---
+++ BusinessLogic/Repository/RepositoryClasses/CompanyRepository.cs
@@ -0,0 +1,41 @@
+﻿using BusinessLogic.Repository.RepositoryInterfaces;
+using DataLayer.Models.Enums;
+using DataLayer.Models;
+using DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Repository.RepositoryClasses
+{
+    public class CompanyRepository : GenericRepository<Company>, ICompanyRepository
+    {
+
+        AppDbContext Context;
+        public CompanyRepository(AppDbContext context) : base(context)
+        {
+            Context = context; ;
+        }
+
+        public IEnumerable<Company> SearchCompanies(string searchTerm, CompanyType? companyType)
+        {
+            var query = Context.Companies.AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(searchTerm))
+            {
+                query = query.Where(c => c.Name.Contains(searchTerm) || c.Description.Contains(searchTerm));
+            }
+
+            if (companyType.HasValue)
+            {
+                query = query.Where(c => c.CompanyType == companyType);
+            }
+
+            return query.ToList();
+        }
+
+
+    }
+}
