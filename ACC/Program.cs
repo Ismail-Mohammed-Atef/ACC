1648cb9c31d9283bd974bb30cc66bc63a18a78c2
Ahmed Nasr <amednasr164@gmail.com>
2025-03-11 12:46:23 +0200

    masterCleaned

--- ACC/Program.cs
+++ ACC/Program.cs
@@ -26,7 +26,6 @@ namespace ACC
             #region Dependency Injection Repositories
 
             builder.Services.AddScoped<IProjetcRepository, ProjectRepository>();
-            builder.Services.AddScoped<ICompanyRepository, CompanyRepository>();
 
             #endregion
 
