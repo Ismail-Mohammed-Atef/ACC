2eaf5696e7e65ff59e83177e00d29192db1408be
Gaber Raafat <gaberraffat8@gmail.com>
2025-03-12 01:56:57 +0200

    gaber new

--- DataLayer/Models/Enums/CompanyType.cs
+++ DataLayer/Models/Enums/CompanyType.cs
@@ -1,32 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
 
 namespace DataLayer.Models.Enums
 {
     public enum CompanyType
     {
+        [Description("Architecture")]
         Architecture,
-        Communications,
-        Communications_Data,
-        Concrete,
-        Concrete_CastInPlace,
-        Concrete_Precast,
-        ConstructionManagement,
-        ConveyingEquipment,
-        ConveyingEquipment_Elevators,
-        Demolition,
-        Earthwork,
-        Earthwork_SiteExcavationGrading,
-        Electrical,
-        ElectricalPowerGeneration,
-        ElectronicSafetySecurity,
-        Equipment,
-        Equipment_KitchenAppliances,
-        ExteriorImprovements,
-        Exterior_FencesGates
 
+        [Description("Communications")]
+        Communications,
+
+        [Description("Communications Data")]
+        Communications_Data,
+
+        [Description("Concrete")]
+        Concrete,
+
+        [Description("Concrete Cast In Place")]
+        Concrete_CastInPlace,
+
+        [Description("Concrete Precast")]
+        Concrete_Precast,
+
+        [Description("Construction Management")]
+        ConstructionManagement,
+
+        [Description("Conveying Equipment")]
+        ConveyingEquipment,
+
+        [Description("Conveying Equipment Elevators")]
+        ConveyingEquipment_Elevators,
+
+        [Description("Demolition")]
+        Demolition,
+
+        [Description("Earthwork")]
+        Earthwork,
+
+        [Description("Earthwork Site Excavation Grading")]
+        Earthwork_SiteExcavationGrading,
+
+        [Description("Electrical")]
+        Electrical,
+
+        [Description("Electrical Power Generation")]
+        ElectricalPowerGeneration,
+
+        [Description("Electronic Safety Security")]
+        ElectronicSafetySecurity,
+
+        [Description("Equipment")]
+        Equipment,
+
+        [Description("Equipment Kitchen Appliances")]
+        Equipment_KitchenAppliances,
+
+        [Description("Exterior Improvements")]
+        ExteriorImprovements,
+
+        [Description("Exterior Fences Gates")]
+        Exterior_FencesGates
     }
 }
---
+++ Helpers/EnumHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+
+namespace Helpers
+{
+    public static class EnumHelper
+    {
+        public static string GetDescription(Enum value)
+        {
+            var field = value.GetType().GetField(value.ToString());
+            var attribute = field.GetCustomAttributes(typeof(DescriptionAttribute), false)
+                                .FirstOrDefault() as DescriptionAttribute;
+            return attribute?.Description ?? value.ToString();
+        }
+
+    }
+}
