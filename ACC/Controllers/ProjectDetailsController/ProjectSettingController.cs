2d55ad4741fe3ec9f7c799646ec3f02619bde4f1
Gaber Raafat <gaberraffat8@gmail.com>
2025-03-20 12:48:57 +0200

    creating a folder for project controllers

---
+++ ACC/Controllers/ProjectDetailsController/ProjectSettingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ACC.Controllers.ProjectDetailsController
+{
+    public class ProjectSettingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
