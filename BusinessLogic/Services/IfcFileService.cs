ccc63be3360156f5702eeae9511a50b0f473fd87
Gaber Raafat <gaberraffat8@gmail.com>
2025-05-24 21:34:07 +0300

    Gaber new 15 5 (#49)
    * First pull from the new project clone
    
    * update the layouts (made the accound admin in the header route to the home page of the prokect)
    
    * setting the main structure for the IFC viewer
    
    * completing the viewer and working fine (not suporting loading ifc file yet)
    
    * try number 2 for fixing ifc loader
    
    * try number 3 for ifc loader

---
+++ BusinessLogic/Services/IfcFileService.cs
@@ -0,0 +1,44 @@
+﻿using BusinessLogic.Repository.RepositoryClasses;
+using DataLayer.Models;
+using Helpers;
+using Microsoft.AspNetCore.Http;
+
+namespace BusinessLogic.Services
+{
+    public class IfcFileService
+    {
+        private readonly IfcFileRepository _ifcFileRepository;
+        private readonly FileHelper _fileHelper;
+
+        public IfcFileService(IfcFileRepository ifcFileRepository, FileHelper fileHelper)
+        {
+            _ifcFileRepository = ifcFileRepository;
+            _fileHelper = fileHelper;
+        }
+
+        public async Task<List<IfcFile>> GetIfcFilesByProjectIdAsync(int projectId)
+        {
+            return await _ifcFileRepository.GetByProjectIdAsync(projectId);
+        }
+
+        public async Task<IfcFile> UploadIfcFileAsync(IFormFile file, int projectId, string userId)
+        {
+            var filePath = await _fileHelper.SaveIfcFileAsync(file);
+            var ifcFile = new IfcFile
+            {
+                FileName = file.FileName,
+                FilePath = filePath,
+                ProjectId = projectId,
+                UploadedBy = userId,
+                UploadDate = DateTime.UtcNow
+            };
+            await _ifcFileRepository.AddAsync(ifcFile);
+            return ifcFile;
+        }
+
+        public async Task<IfcFile> GetIfcFileByIdAsync(int id)
+        {
+            return await _ifcFileRepository.GetByIdAsync(id);
+        }
+    }
+}
