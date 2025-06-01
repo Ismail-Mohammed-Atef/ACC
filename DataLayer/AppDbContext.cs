00744e2a7b83ab66e58426d09e6691e7f29d579f
Ismail-Mohammed-Atef <ismail.mohammed.atef@gmail.com>
2025-06-01 18:05:56 +0300

    co

--- DataLayer/AppDbContext.cs
+++ DataLayer/AppDbContext.cs
@@ -143,6 +143,9 @@ namespace DataLayer
         public DbSet<Role> Roles { get; set; }
         public DbSet<ProjectActivities> ProjectActivities { get; set; }
         public DbSet<ProjectCompany> ProjectCompany { get; set; }
+        public DbSet<IfcFile> IfcFiles { get; set; }
+        public DbSet<Transmittal> Transmittals { get; set; }
+        public DbSet<TransmittalDocument> TransmittalDocuments { get; set; }
 
         public DbSet<Review> Reviews { get; set; }
         public DbSet<WorkflowTemplate> WorkflowTemplates { get; set; }
